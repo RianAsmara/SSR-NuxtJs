e48a976ee99d2a2068ce67b104bebb40ecee0ae5
RianAsmara <rputra711@gmail.com>
2018-03-11 22:46:45 +0800

    1st Commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <h1>Search iTunes</h1>
+<br/>
+<form @submit.prevent="submit">
+  <input type="text" v-model="search" placeholder="Enter Artist Name" 
+  autofocus >
+</form>
+</div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+      search: ''
+    }
+  },
+  methods: {
+    submit(event) {
+      this.$router.push(`results/${this.search}`);
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  text-align: center;
+}
+h1 {
+  padding: 20px;
+}
+</style>
